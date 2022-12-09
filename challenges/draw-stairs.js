da0939ece81f35793dbb90ed2afe1a3d960b17ca
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2022-12-09 11:02:41 -0800

    completed hh 3

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -19,67 +19,61 @@ drawStairs(6) ->
 
 const drawStairs = n => {
      if (n <= 0) return;
-     str = "";
      for (let i = 1; i <= n; i++) {
-          str += " ".repeat(n-i) + "*".repeat(i) + "\n";
+          console.log(" ".repeat(n-i) + "*".repeat(i));
      }
-     return str;
-};
+ };
 
-
-/* 
-
-Extension:
-Write a function that logs to the console an nxn overlapping '+' and 'X' for a given 
-number n where n must be an odd positive number. Note that each row of the star must
-be 'n' characters long. Be sure to include any leading/trailing spaces per row.
-
-Examples:
-drawStar(1) ->
-+
-
-drawStar(3) ->
-\|/
--+-
-/|\
-
-drawStar(5) ->
-\ | /
+ /* 
+ 
+ Extension:
+ Write a function that logs to the console an nxn overlapping '+' and 'X' for a given 
+ number n where n must be an odd positive number. Note that each row of the star must
+ be 'n' characters long. Be sure to include any leading/trailing spaces per row.
+ 
+ Examples:
+ drawStar(1) ->
+ +
+ 
+ drawStar(3) ->
+ \|/
+ -+-
+ /|\
+ 
+ drawStar(5) ->
+ \ | /
  \|/ 
---+--
+ --+--
  /|\ 
-/ | \
-
-*/
-
-const drawStar = n => {
-if (n%2 === 0) return;
-let str = "";
-for (let i = 1; i <= n; i++) {
-
-     if (i < n/2 || i > (n/2)+1) {
-          for (let j = 1; j <= n; j++) {
-               if (j === i) str += "\\";
-               else if (j === n-(i-1)) str+="/";
-               else if (j < Math.round(n/2)|| j > Math.round(n/2)) str+= " ";
-               if (j === Math.round(n/2)) str+= "|";
-               if (j === n) str+= "\n";
+ / | \
+ 
+ */
+ 
+ const drawStar = n => {
+     if (n%2 === 0) return;
+     
+     for (let i = 1; i <= n; i++) {
+          let str = "";
+          if (i < n/2 || i > (n/2)+1) {
+               for (let j = 1; j <= n; j++) {
+                    if (j === i) str += "\\";
+                    else if (j === n-(i-1)) str+="/";
+                    else if (j < Math.round(n/2)|| j > Math.round(n/2)) str+= " ";
+                    if (j === Math.round(n/2)) str+= "|";
+               }
           }
-     }
-
-
-     if (i === Math.round(n/2)) {
-          for (let j = 1; j <= n; j++) {
-               if (j < n/2 || j > Math.round(n/2)) str += "-";
-               if (j === Math.round(n/2)) str += "+";
-               if (j === n) str+= "\n";
+     
+     
+          if (i === Math.round(n/2)) {
+               for (let j = 1; j <= n; j++) {
+                    if (j < n/2 || j > Math.round(n/2)) str += "-";
+                    if (j === Math.round(n/2)) str += "+";
+               }
           }
+          console.log(str); 
      }
-}
-return str;  
-}
-
-
-module.exports = { drawStairs, drawStar };
-
-
+ 
+ }
+ 
+ module.exports = { drawStairs, drawStar };
+ 
