d79347eecad1446d6c56361bd6b67d7316cbbc5c
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2022-12-17 09:52:18 -0800

    completed HH 8

--- challenges/common-elements.js
+++ challenges/common-elements.js
@@ -15,10 +15,33 @@ arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
 commonElements(arr1, arr2, arr3) -> [2, 3, 2000, 'dog']
 */
 
-const commonElements = (...args) => {
+const commonElements = (...args) => {        //O(n)?
+
+    const cache = new Map;
+    const result = [];
+
+    for (let i = 0; i < args.length; i++) {
+        args[i].reduce((pre, cur) => {
+            if (pre[cur]) pre[cur]++
+            else pre[cur] = 1;
+            return pre;
+        }, cache)
+    }
+
+    for (const key in cache) {
+        if (cache[key] === args.length) result.push(key)
+    }
+
+
+    return result;
 
 }
 
+const arr1 = [2, 10,'cat', 3, 99, 2000, 'dog', 'lion'];
+const arr2 = [3, 7, 2, 2000, 1, 'dog', 'cat'];
+const arr3 = [2, 100, 2000, 'dog', 3, 'lion'];
+
+console.log(commonElements(arr1, arr2, arr3));
 
 
 /*
