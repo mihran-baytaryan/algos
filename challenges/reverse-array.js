cdcd9ec6f6597a4be83d1dfbaaefee26f50c7d66
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-01-11 09:14:23 -0800

    01/11/2022

--- challenges/reverse-array.js
+++ challenges/reverse-array.js
@@ -7,7 +7,19 @@ to the array prototype while solving the problem.
 */
 
 const reverseArray = array => {
-  
+
+    let counter = 0;
+
+    while (counter < Math.floor(array.length/2)){
+
+        [array[counter], array[array.length - (counter + 1)]] = [array[array.length - (counter + 1)], array[counter]];
+
+        counter++
+
+    }
+
+    return array;
+
 };
 
 /*
@@ -24,8 +36,20 @@ input string will always have at least one word
 
 */
 
-const reverseSentence = sentence => {
+const reverseSentence = sentence => {       //doing in place mutation but with reassignment, would be issue if arugment is const. can do in different ways
+
+    const array = sentence.match(/[^\s]+/g);
+    array
+    sentence = '';
+    for (let i = array.length -1; i >= 0; i --) {
+        sentence += array[i];
+        if (i > 0) sentence += " ";
+    }
+
+    return sentence;
   
 };
 
 module.exports = {reverseArray, reverseSentence};
+
+console.log(reverseSentence('teriyaki chicken'));
