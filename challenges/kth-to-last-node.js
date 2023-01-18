56e65060e7b8301852419514a56274465ce59dda
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-01-17 23:13:52 -0800

    01/17/2022

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -25,9 +25,57 @@ function Node(val) {
   this.next = null;
 }
 
-function kthToLastNode(k, head){
+// function kthToLastNode(k, head){      //iterative approach
+//   if (!k||!head) return;
 
+//   let counter = 0;
+//   let node = head;
+
+//   while (node) {
+//     counter++;
+//     node.index = counter;
+//     node = node.next;
+//   }
+
+//   node = head;
+//   while(node) {
+//     if (node.index === (counter - (k-1))) return node.value;
+//     node = node.next;
+//   }
+  
+// }
+
+function kthToLastNode(k, head){                 //recursive approach
+  if (!k||!head) return;
+
+  function inner(node, counter=0, length) {
+    if (node === null) {
+      length = counter;
+      return length;
+    }
+    counter ++;
+    length = inner(node.next, counter);
+    if (length - (k-1) <= 0) return;
+    if (counter === (length - (k-1))) return node.value;
+    else return length;
+  }
+
+  return inner(head)
 }
+
 
 //Do not delete! 
 module.exports = {Node, kthToLastNode};
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
+// console.log(kthToLastNode(1, a))
