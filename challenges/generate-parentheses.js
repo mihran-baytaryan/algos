75b4ff13445d513de3508437a69a52170fe59a6a
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-02-16 09:22:11 -0800

    02/15/2023

--- challenges/generate-parentheses.js
+++ challenges/generate-parentheses.js
@@ -29,7 +29,21 @@ Given n = 0, a solution set is:
 */
 
 const generateParentheses = n => {
+  const results = [];
   
+  const generate = (str, l, r) => {
+    if (r === 0) {
+      results.push(str);
+      return;
+    }
+
+    if (l > 0) generate(str + '(', l - 1, r);
+
+    if (l < r) generate(str + ')', l, r - 1);
+  }  
+  
+  generate('', n, n);
+  return results;
 };
 
 module.exports = {generateParentheses};
