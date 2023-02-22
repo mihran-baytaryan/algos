56c128fcf045abda33ab1e16b3de7ddc7041c947
Mihran Baytaryan <mihranbaytaryan@gmail.com>
2023-02-22 01:05:40 -0800

    02/21/2023

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -14,26 +14,39 @@
  * Hint: Think about the length of the string and how that relates to the frequencies of the characters
  */
 
-const permPalin = str => {
-  if (typeof str !== 'string') return false;
+// const permPalin = str => {
+//   if (typeof str !== 'string') return false;
 
-  const cache = [...str].reduce((pre, cur) => {
-    if (pre[cur]) delete pre[cur];
-    else pre[cur] = 1;
-    return pre;
-  }, {})
+//   const cache = [...str].reduce((pre, cur) => {
+//     if (pre[cur]) delete pre[cur];
+//     else pre[cur] = 1;
+//     return pre;
+//   }, {})
 
-  if (str.length % 2 !== 0 && Object.values(cache).length > 1) return false;
-  if (str.length % 2 === 0 && Object.values(cache).length) return false;
+//   if (str.length % 2 !== 0 && Object.values(cache).length > 1) return false;
+//   if (str.length % 2 === 0 && Object.values(cache).length) return false;
   
-  return true;
-};
+//   return true;
+// };
 
 /* 
  * Extension: Solve in constant space complexity.
  */
-const permPalinAdvanced = str => {
-
+const permPalin = str => { //Advanced
+  if (typeof str !== 'string') return false;
+  let oddCounter = 0;
+  for (let i = 0; i < str.length - 1; i++) {
+    let charCounter = false;
+    for (let j = 0; j < str.length; j++) {
+      if (str[i] === str[j]){
+        if (charCounter) charCounter = false;
+        else charCounter = true;
+      }
+    }
+    if (charCounter) oddCounter += 1;
+    if (str.length % 2 == 0 && oddCounter) return false;
+    if (oddCounter > 2) return false;
+  }
+  return true;
 };
-
 module.exports = { permPalin };
